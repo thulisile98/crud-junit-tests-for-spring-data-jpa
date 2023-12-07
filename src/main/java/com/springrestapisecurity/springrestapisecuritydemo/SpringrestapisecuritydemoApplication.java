ffb6787346c1b748543ce273a8b4c6824a5b9071
Angel Mashego <thulisile.ma98@gmail.com>
2023-12-07 15:23:30 +0200

    initial commit

---
+++ src/main/java/com/springrestapisecurity/springrestapisecuritydemo/SpringrestapisecuritydemoApplication.java
@@ -0,0 +1,13 @@
+package com.springrestapisecurity.springrestapisecuritydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestapisecuritydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestapisecuritydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrestapisecurity/springrestapisecuritydemo/SpringrestapisecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrestapisecurity.springrestapisecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestapisecuritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
